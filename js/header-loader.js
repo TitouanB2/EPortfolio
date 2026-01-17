35ea9b68b699911ab7dde1d593667c5f078c0c2e
TitouanB2 <titouan.barry@etu.univ-smb.fr>
2026-01-17 23:24:24 +0100

    fix url

--- js/header-loader.js
+++ js/header-loader.js
@@ -1,5 +1,5 @@
 // Charge le header dynamiquement dans toutes les pages
-fetch('header.html')
+fetch('../Header/header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('header-placeholder').innerHTML = data;
